35a0b40a78607bc276f14ea1eaff4c172b64d10a
derekxi2021 <88201015+derekxi2021@users.noreply.github.com>
2026-06-06 10:41:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,12 +29,12 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 #git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
 
 # set golang 1.25.x
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/kenzok8/golang -b 1.25 feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang -b 1.25 feeds/packages/lang/golang
 
 # set golang 1.24.x (main)
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # set golang 1.23.x
 #rm -rf feeds/packages/lang/golang
